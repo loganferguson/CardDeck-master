523e4515be657a7d1bc2498aa689cb8100298a53
Logan Ferguson <lferg1292@yahoo.com>
2020-02-13 21:12:35 -0500

    Add files via upload

---
+++ Deck.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+
+namespace CardDeck
+{
+    class Deck
+    {
+        List<Card> Cards { get; set; }
+        public static List<Card> Hand1 = new List<Card>();
+        public static List<Card> Hand2 = new List<Card>();
+        public int NumCards { get => Cards.Count; }
+
+        public Deck()
+        {
+            Cards = new List<Card>();
+        }
+
+        public void AddCard(Card newCard)
+        {
+            Cards.Add(newCard);
+        }
+
+        public Card RemoveTopCard()
+        {
+            Card cardToRemove = Cards[0];
+            Cards.RemoveAt(0);
+            return cardToRemove;
+        }
+
+        public void PrintDeck()
+        {
+            foreach(Card c in Cards)
+            {
+                System.Console.WriteLine(c);
+            }
+        }
+
+        public Card PrintYourCard(int handIndex)
+        {
+            return Hand1[handIndex];
+        }
+
+        public Card PrintEnemyCard(int handIndex)
+        {
+            return Hand2[handIndex];
+        }
+
+        public void PrintWar(int handIndex)
+        {
+            Console.WriteLine("----facedown cards----");
+            Console.WriteLine("----facedown cards----");
+            Console.WriteLine($"You: {Hand1[handIndex]}");
+            Console.WriteLine($"Enemy: {Hand2[handIndex]}");
+        }
+
+        public void ShuffleDeck()
+        {
+            Random r = new Random();
+
+            for (int i = 0; i < 10000 ; i++)
+            {
+                int min = 0;
+                int max = 52;
+                int randomPlace = (r.Next(min, max));
+                Card randomCard = Cards[randomPlace];
+                this.Cards.Add(randomCard);
+                this.Cards.RemoveAt(randomPlace);
+            }
+        }
+
+        public void Deal()
+        {
+            for (int i = 0; i < NumCards; i+=2)
+            {
+                Hand1.Add(Cards[i]);
+                Hand2.Add(Cards[i+1]);
+            }
+        }
+
+        public void SortDeck()
+        {
+            Cards.Sort();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CardDeck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck d = new Deck();
+            
+
+            foreach(CardSuit cs in Enum.GetValues(typeof(CardSuit)))
+            {
+                foreach(CardValue cv in Enum.GetValues(typeof(CardValue)))
+                {
+                    d.AddCard(new Card(cs, cv));
+                }
+            }
+                DisplayMenu();
+                string menuSelect = Console.ReadLine();
+
+            // THIS IS THE GAME PLAY STUFF
+
+         if (menuSelect == "w" || menuSelect == "W")
+            {
+                d.ShuffleDeck();
+                d.Deal();
+                bool stillPlaying = true;
+                int handIndex = 0;
+                while (stillPlaying == true && Deck.Hand1.Count > 0 && Deck.Hand2.Count > 0)
+                {
+                    Console.Write($"\nEnter (p) to play card: ");
+                    string playInput = Console.ReadLine();
+                    Console.WriteLine("");
+                    if (playInput== "p" || playInput == "P")
+                    {
+                        Console.WriteLine($"Your Card: {d.PrintYourCard(handIndex)}");
+                        Console.WriteLine($"Enemy Card: {d.PrintEnemyCard(handIndex)}");
+                        if (Deck.Hand1[handIndex] > Deck.Hand2[handIndex])
+                        {
+                            Console.WriteLine("--------Round won!--------");
+                            Deck.Hand1.Add(Deck.Hand2[handIndex]);
+                        }
+                        else if (Deck.Hand1[handIndex] < Deck.Hand2[handIndex])
+                        {
+                            Console.WriteLine("--------Round lost--------. :( ");
+                            Deck.Hand2.Add(Deck.Hand1[handIndex]);
+                        }
+                        else if (Deck.Hand1[handIndex] == Deck.Hand2[handIndex])
+                        {
+                            bool war = true;
+                            while (war == true)
+                            {
+                                Console.WriteLine("--------WAR!!!--------");
+                                handIndex += 3;
+                                d.PrintWar(handIndex);
+                                if (Deck.Hand1[handIndex] > Deck.Hand2[handIndex])
+                                {
+                                    Console.WriteLine("----YOU WON THE WAR!----");
+                                    Deck.Hand1.Add(Deck.Hand2[handIndex]);
+                                    Deck.Hand1.Add(Deck.Hand2[handIndex - 1]);
+                                    Deck.Hand1.Add(Deck.Hand2[handIndex - 2]);
+                                    war = false;
+                                }
+                                else if (Deck.Hand1[handIndex] < Deck.Hand2[handIndex])
+                                {
+                                    Console.WriteLine("----YOU LOST THE WAR!----");
+                                    Deck.Hand2.Add(Deck.Hand1[handIndex]);
+                                    Deck.Hand2.Add(Deck.Hand1[handIndex - 1]);
+                                    Deck.Hand2.Add(Deck.Hand1[handIndex - 2]);
+                                    war = false;
+                                }
+                            }
+                        }
+                        
+                    }
+
+                    handIndex++;
+                }         
+            }           
+        }    
+        static void DisplayMenu()
+            {
+                Console.WriteLine("  -----       GAME MENU        -----   ");
+                Console.WriteLine("   -----                      -----    ");
+                Console.WriteLine("    ---Enter (W) to declare WAR!--     ");
+                Console.WriteLine("     ----         --         ----      ");
+                Console.WriteLine("      ---- Enter (E) to exit----       ");
+                Console.WriteLine("       ----     ------     ----        ");
+                Console.WriteLine("        ----   --------   ----         ");
+                Console.WriteLine("         ---- ----  ---- ----          ");
+                Console.WriteLine("          -------    -------           ");
+                Console.WriteLine("           -----      -----            ");
+            }
+
+    }
+}
